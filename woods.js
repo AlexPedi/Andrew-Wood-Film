dc9a216de2d19f9d8d1236fe1e995a963cb51f16
Alex Pedicini <alexpedicini@Alexs-MacBook-Pro-3.local>
2019-01-05 20:27:58 -0500

    update to enoch page

--- woods.js
+++ woods.js
@@ -1,12 +1,12 @@
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+// var header = document.getElementById("myDIV");
+// var btns = header.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
 
 $('#contactModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
@@ -14,6 +14,11 @@ $('#contactModal').on('show.bs.modal', function (event) {
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
 })
+
+$('img').on('click',function()
+            {
+                var sr=$(this).attr('src'); 
+                $('#mimg').attr('src',sr);
+                $('#myModal').modal('show');
+            });
